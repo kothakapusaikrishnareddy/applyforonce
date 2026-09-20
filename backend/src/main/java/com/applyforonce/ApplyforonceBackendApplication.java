4d55f2b3551c778d35ea187721422feb98924541
kothakapusaikrishnareddy <kothakapusaikrishnareddy@gmail.com>
2026-09-20 17:55:54 +0530

    chore: initialize ApplyForOnce backend

---
+++ backend/src/main/java/com/applyforonce/ApplyforonceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.applyforonce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyforonceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplyforonceBackendApplication.class, args);
+	}
+
+}
